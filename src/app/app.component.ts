1b1632d25052f516150ddbaff8d9d9ac9ab316a9
weiyan.Lu <weiyan.Lu@cn.bosch.com>
2021-04-02 07:39:19 +0800

    Add new properties @Input & @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TopBars } from './components';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,49 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pinduoduo';
 
+  topBars: TopBars[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '女装',
+      link: ''
+    },
+    {
+      title: '鞋包',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    },
+    {
+      title: '汽车',
+      link: ''
+    },
+    {
+      title: '美食',
+      link: ''
+    },
+    {
+      title: '儿童',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    }
+  ];
 
+
+  handleTabSelected(tabMenu: TopBars) {
+    console.log(tabMenu);
+  }
 }
 
 
--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+export interface TopBars {
+  title: string;
+  link: string;
+};
 
 @Component({
   selector: 'app-scrollable-tab',
   templateUrl: './scrollable-tab.component.html',
   styleUrls: ['./scrollable-tab.component.css']
 })
+
 export class ScrollableTabComponent implements OnInit {
 
-  public topBars = ['热门', '女装', '男装', '鞋包', '家装', '汽车', '美食', '儿童', '母婴'];
+  selectedIndex = -1;
   constructor() { }
 
+  @Input() menus: TopBars[] = [];
+  @Input() backgroundColor = '#fff';
+  @Input() menuActiveColor = 'black';
+  @Input() menuColor = 'white';
+  @Output() tabSelected = new EventEmitter();
+
   ngOnInit() {
+  }
+
+  hanleSelection(index: number) {
+    this.selectedIndex = index;
+    this.tabSelected.emit(this.menus[this.selectedIndex]);
   }
 
 }
